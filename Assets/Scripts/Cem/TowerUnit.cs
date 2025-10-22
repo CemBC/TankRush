b3a49f9bcce19b033f8a7cf80c40c0822589584d
Cem Başar Ceylani <cembasar07@gmail.com>
2025-10-22 17:02:59 +0300

    Spride renderer for destroying or upgrading unit

--- Assets/Scripts/Cem/TowerUnit.cs
+++ Assets/Scripts/Cem/TowerUnit.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 [RequireComponent(typeof(SphereCollider))]
-public class TowerUnit : MonoBehaviour
+public class TowerUnit : MonoBehaviour, IPointerClickHandler
 {
-    public TowerData data; 
+    Vector3 xInitPos, uInitPos;
+    Quaternion xInitRot, uInitRot;
+    public GameObject XIcon;
+    public GameObject UpgradeIcon;
+    
+    bool uiVisible = false;
+    int  suppressClickFrame = -1;  
+
+    public TowerData data;
     public GameObject projectilePrefab;
     public float projectileSpeed;
     public float projectileLifeTime;
@@ -21,7 +31,21 @@ public class TowerUnit : MonoBehaviour
     {
         rangeCollider = GetComponent<SphereCollider>();
         rangeCollider.isTrigger = true;
-        rangeCollider.radius =data.range;
+        rangeCollider.radius = data.range;
+        rangeCollider.enabled = false;
+        if (XIcon)
+        {
+            xInitPos = XIcon.transform.position;
+            xInitRot = XIcon.transform.rotation;
+            XIcon.gameObject.SetActive(false);
+        }
+        if (UpgradeIcon)
+        {
+            uInitPos = UpgradeIcon.transform.position;
+            uInitRot = UpgradeIcon.transform.rotation;
+            UpgradeIcon.gameObject.SetActive(false);
+        }
+
     }
 
     void OnEnable()
@@ -31,6 +55,63 @@ public class TowerUnit : MonoBehaviour
             return;
         }
         shootCoroutine = StartCoroutine(ShootLoop());
+
+
+    }
+
+     public void OnPointerClick(PointerEventData e)
+    {
+        if (e.button != PointerEventData.InputButton.Left) return;
+        if (Time.frameCount == suppressClickFrame) return;
+
+        if (!uiVisible)
+        {
+            ShowUI();
+        }
+        
+    }
+
+    void Update()
+    {
+        if (!uiVisible || Mouse.current == null) return;
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            if (RayHitsObject(XIcon))
+            {
+                Destroy(gameObject);
+                return;
+            }
+            if (RayHitsObject(this.gameObject))
+                return;
+
+            HideUI();
+            suppressClickFrame = Time.frameCount;
+        }
+    }
+    void LateUpdate()
+    {
+        if (!uiVisible) return;
+
+        if (XIcon)
+        {
+            XIcon.transform.position = xInitPos;
+            XIcon.transform.rotation = xInitRot;
+        }
+        if (UpgradeIcon)
+        {
+            UpgradeIcon.transform.position = uInitPos;
+            UpgradeIcon.transform.rotation = uInitRot;
+        }
+
+
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            if (!RayHitsObject(gameObject))
+            {
+                HideUI();
+            }
+        }
     }
 
     void OnDisable()
@@ -77,7 +158,7 @@ public class TowerUnit : MonoBehaviour
     Transform PickNearestInRange()
     {
         Collider[] hits = Physics.OverlapSphere(transform.position, rangeCollider.radius, enemyMask);
-        float best = float.MaxValue; 
+        float best = float.MaxValue;
         Transform final = null;
         Vector3 towerPosition = transform.position;
 
@@ -88,7 +169,7 @@ public class TowerUnit : MonoBehaviour
             if (distance < best)
             {
                 best = distance;
-                final = hits[i].transform; 
+                final = hits[i].transform;
             }
         }
         return final;
@@ -106,4 +187,55 @@ public class TowerUnit : MonoBehaviour
         if (!projectile) projectile = gameObject.AddComponent<Projectile>();
         projectile.Init(direction, projectileSpeed, projectileLifeTime, projectileRadius, projectilePierce, enemyMask, data.range);
     }
+
+
+    void ShowUI()
+    {
+       if (XIcon)
+        {
+            XIcon.transform.position = xInitPos;
+            XIcon.transform.rotation = xInitRot;
+            XIcon.gameObject.SetActive(true);
+        }
+        if (UpgradeIcon)
+        {
+            UpgradeIcon.transform.position = uInitPos;
+            UpgradeIcon.transform.rotation = uInitRot;
+            UpgradeIcon.gameObject.SetActive(true);
+        }
+        uiVisible = true;
+    }
+
+    void HideUI()
+    {
+        if (XIcon) XIcon.SetActive(false);
+        if (UpgradeIcon) UpgradeIcon.SetActive(false);
+        uiVisible = false;
+    }
+
+    bool RayHitsObject(GameObject target)
+    {
+        if (target == null) return false;
+
+        var cam   = Camera.main;
+        var mouse = Mouse.current;
+
+        if (cam == null) return false;
+
+         Vector2 screenPos;
+        if (mouse != null)
+            screenPos = mouse.position.ReadValue();
+        else if (Touchscreen.current != null)
+            screenPos = Touchscreen.current.primaryTouch.position.ReadValue();
+        else
+            return false;
+
+        Ray ray = cam.ScreenPointToRay(screenPos);
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000f, ~0, QueryTriggerInteraction.Ignore))
+        {
+            return hit.transform == target.transform || hit.transform.IsChildOf(target.transform);
+        }
+        return false;
+    }
+
 }
