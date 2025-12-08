e483d243dfa36f10263686e1d33d6271f17b4798
Cem Başar Ceylani <cembasar07@gmail.com>
2025-12-08 17:56:22 +0300

    half size of a tile offset has been added to camera auto fitter in case of visualization of sprite renderers

--- Assets/Scripts/Cem/CameraAutoFit.cs
+++ Assets/Scripts/Cem/CameraAutoFit.cs
@@ -14,8 +14,8 @@ public class CameraAutoFit : MonoBehaviour
     public void Fit()
     {
         if (cam == null || level == null 
-        || level.terrainRows == null 
-        || level.terrainRows.Length == 0)
+            || level.terrainRows == null 
+            || level.terrainRows.Length == 0)
         {
             Debug.LogError("Camera or Level missing");
             return;
@@ -23,20 +23,19 @@ public class CameraAutoFit : MonoBehaviour
 
         int height = level.terrainRows.Length;
         int width = CountCells(level.terrainRows[0]);
-
         float worldWidth = width * tileSize;
         float worldHeight = height * tileSize;
-
-        Vector3 center = new Vector3((width - 1) * tileSize * 0.5f, 0f,
-        (height - 1) * tileSize * 0.5f);
+        Vector3 center = new Vector3((width - 1) * tileSize * 0.5f,0f,(height - 1) * tileSize * 0.5f);
         cam.orthographic = true;
         cam.transform.position = new Vector3(center.x, 10f, center.z);
         cam.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+        float halfBottomTile = tileSize * 0.5f;
+        float sizeByHeight = (worldHeight * 0.5f) + padding + halfBottomTile;
+        float sizeByWidth  = (worldWidth * 0.5f / cam.aspect) + padding;
 
-        float sizeByHeight = (worldHeight * 0.5f) + padding;
-        float sizeByWidth = (worldWidth * 0.5f / cam.aspect) + padding;
         cam.orthographicSize = Mathf.Max(sizeByHeight, sizeByWidth);
     }
+
 
     private int CountCells(string row)
     {
