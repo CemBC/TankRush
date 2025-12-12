3727e72ac0c7b4368228d9970c90f1c106315c8c
Cem Başar Ceylani <cembasar07@gmail.com>
2025-12-12 20:12:29 +0300

    The changes applied

--- Assets/Scripts/Alper/MainMenu/LevelsMenu.cs
+++ Assets/Scripts/Alper/MainMenu/LevelsMenu.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using UnityEditorInternal;
 public class LevelsMenu : MonoBehaviour
 {
     [Header("Panel Reference")]
@@ -20,37 +20,31 @@ public class LevelsMenu : MonoBehaviour
     void Awake()
     {
         levelButtons = buttonsParent.GetComponentsInChildren<Button>();
+
         SetupButtons();
     }
 
     void SetupButtons()
+{
+    int lastUnlocked = PlayerPrefs.GetInt(LastUnlockedKey, 1);
+
+    for (int i = 0; i < levels.Length && i < levelButtons.Length; i++)
     {
-        int lastUnlocked = PlayerPrefs.GetInt(LastUnlockedKey, 1);
+        LevelData data = levels[i];
+        Button btn = levelButtons[i];
 
-        for (int i = 0; i < levels.Length && i < levelButtons.Length; i++)
-        {
-            LevelData data = levels[i];
-            Button btn = levelButtons[i];
+        if (data == null || btn == null)
+            continue;
 
-            if (data == null || btn == null)
-                continue;
+        bool unlocked = data.levelNumber <= lastUnlocked;
+        btn.interactable = unlocked;
 
-            bool unlocked = data.levelNumber <= lastUnlocked;
-            btn.interactable = unlocked;
-
-            // OnClick'leri sıfırla ve yeniden ekle
-            btn.onClick.RemoveAllListeners();
-
-            if (unlocked)
-            {
-                LevelData captured = data;
-                btn.onClick.AddListener(() =>
-                {
-                    LoadLevel(captured);
-                });
-            }
-        }
+        // En kritik kısım
+        btn.onClick.RemoveAllListeners();
+        btn.onClick.AddListener(() => LoadLevel(data));
     }
+}
+
 
     public void OpenLevelsMenu()
     {
@@ -64,17 +58,14 @@ public class LevelsMenu : MonoBehaviour
 
     public void LoadLevel(LevelData data)
     {
-        Debug.Log("LoadLevel: " + data.levelNumber);
         LevelRuntimePasser.Current = data;
         SceneManager.LoadScene("GameScene");
     }
-
 
     public void OnPlayButtonClicked()
     {
         int lastUnlocked = PlayerPrefs.GetInt(LastUnlockedKey, 1);
         LevelData target = null;
-
         foreach (var data in levels)
         {
             if (data != null && data.levelNumber == lastUnlocked)
@@ -97,10 +88,6 @@ public class LevelsMenu : MonoBehaviour
         if (target != null)
         {
             LoadLevel(target);
-        }
-        else
-        {
-            Debug.LogError("Hiç level bulunamadı, Levels arrayini kontrol et.");
         }
     }
 }
